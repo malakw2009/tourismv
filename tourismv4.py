db2e925c27a54e185801f302ee651d761f9ab377
malakw2009 <167004516+malakw2009@users.noreply.github.com>
2024-04-16 03:11:25 +0200

    Update tourismv4.py
    fix url issue

--- tourismv4.py
+++ tourismv4.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def open_url(url):
-    st.markdown(f"[Click here to open]({url})")
+    st.link_button()
 
 def open_places_to_visit():
     st.header("Places to Visit")
@@ -20,24 +20,24 @@ def open_places_to_visit():
     }
 
     for city, url in cities_dict.items():
-        st.button(city)
-        open_url(url)
+        st.link_button(city, url)
+        #open_url(url)
 
     st.subheader("Categories")
     st.button("History", on_click=open_history)
-    st.button("Medical", on_click=lambda: open_url("https://www.egypttoursportal.com/blog/medical-tourism-in-egypt/"))
-    st.button("Chillout", on_click=lambda: open_url("https://www.tripadvisor.com/Attraction_Review-g294201-d26686959-Reviews-ChillOut_Egypt-Cairo_Cairo_Governorate.html"))
+    st.link_button("Medical", "https://www.egypttoursportal.com/blog/medical-tourism-in-egypt/")
+    st.link_button("Chillout", "https://www.tripadvisor.com/Attraction_Review-g294201-d26686959-Reviews-ChillOut_Egypt-Cairo_Cairo_Governorate.html")
 
 def open_history():
     st.header("History")
-    st.button("Pharaoh", on_click=lambda: open_url("http://giza.fas.harvard.edu/giza3d/"))
+    st.link_button("Pharaoh", "http://giza.fas.harvard.edu/giza3d/")
     st.button("Religion", on_click=open_religion)
 
 def open_religion():
     st.header("Religion")
-    st.button("Islamic", on_click=lambda: open_url("https://www.theislamicquotes.com/islamic-places-to-visit-in-egypt/"))
-    st.button("Christian", on_click=lambda: open_url("https://www.spiritualtravels.info/spiritual-sites-around-the-world/africa/christian-holy-sites-in-egypt/"))
-    st.button("Pharaoh Religion", on_click=lambda: open_url("https://en.wikipedia.org/wiki/Ancient_Egyptian_religion"))
+    st.link_button("Islamic", "https://www.theislamicquotes.com/islamic-places-to-visit-in-egypt/")
+    st.link_button("Christian", "https://www.spiritualtravels.info/spiritual-sites-around-the-world/africa/christian-holy-sites-in-egypt/")
+    st.link_button("Pharaoh Religion", "https://en.wikipedia.org/wiki/Ancient_Egyptian_religion")
 
 def main():
     st.title("Welcome to Egypt")
@@ -50,13 +50,13 @@ def open_another_place():
     st.header("Start")
 
     # Button for Currency Price Transfer
-    st.button("Currency Price Transfer", on_click=lambda: open_url("https://www.cbe.org.eg/en/economic-research/statistics/exchange-rates"))
+    st.link_button("Currency Price Transfer", "https://www.cbe.org.eg/en/economic-research/statistics/exchange-rates")
 
     # Button for Places to Visit
     st.button("Places to Visit", on_click=open_places_to_visit)
 
     # Button for Culture in Egypt
-    st.button("Culture in Egypt", on_click=lambda: open_url("https://www.britannica.com/place/Egypt/Daily-life-and-social-customs"))
+    st.link_button("Culture in Egypt", "https://www.britannica.com/place/Egypt/Daily-life-and-social-customs")
 
     # Button to Exit the Program
     if st.button("Exit"):
